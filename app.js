fadb6826809d8cd28f96b965b7a5ba10d66d07bf
Igor Olkhov <smartestbox@gmail.com>
2022-12-26 21:22:12 +0100

    Setup Codewars 8 kyu task Our football team has finished the championship.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Codewars 8 kyu task "Will you make it?"
+
 // Description:
+
 // You were camping with your friends far away from home, but when it's time to go back, 
 // you realize that your fuel is running out and the nearest pump is 50 miles away! You 
 // know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
@@ -16,7 +18,9 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //---------------------------------------------------------------------------------------------------------
 
 // Codewars 8 kyu task Century From Year
+
 // Description:
+
 // The first century spans from the year 1 up to and including the year 100, the second century - 
 // from the year 101 up to and including the year 200, etc.
 // Task
@@ -32,7 +36,9 @@ const century = year => year % 100 !== 0 ? Math.trunc(year/100) + 1 : Math.trunc
 //---------------------------------------------------------------------------------------------------------
 
 // Codewars 8 kyu task Reversed Strings
+
 // Description:
+
 // Complete the solution so that it reverses the string passed into it.
 // Examples
 // 'world'  =>  'dlrow'
@@ -50,7 +56,9 @@ const solution = str => {
 //---------------------------------------------------------------------------------------------------------
 
 // Codewars 8 kyu task Opposites Attract
+
 // Description: 
+
 // Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a 
 // flower each. If one of the flowers has an even number of petals and the other has an odd number of petals 
 // it means they are in love.
@@ -70,7 +78,9 @@ function lovefunc(flower1, flower2){
 //---------------------------------------------------------------------------------------------------------
 
 // Codewars 8 kyu task Returning Strings
+
 // Description:
+
 // Make a function that will return a greeting statement that uses an input; your program should return, 
 // "Hello, <name> how are you doing today?".
 
@@ -79,9 +89,37 @@ const greet = name => `Hello, ${name} how are you doing today?`
 //---------------------------------------------------------------------------------------------------------
 
 // Codewars 8 kyu task Remove String Spaces
+
 // Description:
+
 // Simple, remove the spaces from the string, then return the resultant string.
 
 const noSpace = str => str.split(' ').join('')
 
 //---------------------------------------------------------------------------------------------------------
+
+// Codewars 8 kyu task Our football team has finished the championship.
+
+// Description:
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string 
+// in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+    return 0
+  }
