4fad0ce1caf7c6bc652f87a0455f351f416183bb
Igor Olkhov <smartestbox@gmail.com>
2022-12-26 20:43:13 +0100

    Solve Codewars 8 kyu task Returning Strings

--- app.js
+++ app.js
@@ -74,6 +74,4 @@ function lovefunc(flower1, flower2){
 // Make a function that will return a greeting statement that uses an input; your program should return, 
 // "Hello, <name> how are you doing today?".
 
-function greet(name){
-    
-  }
+const greet = name => `Hello, ${name} how are you doing today?`
