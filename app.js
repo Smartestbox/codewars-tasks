b56e601d6dfafa1fb887fd759882a897d54d202f
Igor Olkhov <smartestbox@gmail.com>
2022-12-27 01:47:27 +0100

    Solve Codewars 8 kyu task Grasshopper - Summation

--- app.js
+++ app.js
@@ -142,7 +142,16 @@ const points = games => {
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
-var summation = function (num) {
-    // Code here
-  }
+const summation = num => {
+    let sum = 0;
+    for(let i = num; i > 0; i--) {
+      sum += i;
+    }
+    return sum
+}
+
+//---------------------------------------------------------------------------------------------------------
+
+
+
 
