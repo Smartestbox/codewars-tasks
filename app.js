5b573fdf57252ad3ba9d98818f71b67ea2e8df2d
Igor Olkhov <smartestbox@gmail.com>
2022-12-28 16:58:50 +0100

    Setup Codewars 8 kyu task Count of positives / sum of negatives

--- app.js
+++ app.js
@@ -168,3 +168,20 @@ const summation = num => {
 const past = (h, m, s) => h * 3600000 + m * 60000 + s * 1000
 
 //---------------------------------------------------------------------------------------------------------
+
+// Codewars 8 kyu task Count of positives / sum of negatives
+
+// Description:
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+    // your code here
+  }
