98f598050bf48cfd20693fa3d28de8588dea50c9
Igor Olkhov <smartestbox@gmail.com>
2022-12-28 16:54:27 +0100

    Setup Codewars 8 kyu task Beginner Series #2 Clock

--- app.js
+++ app.js
@@ -152,6 +152,19 @@ const summation = num => {
 
 //---------------------------------------------------------------------------------------------------------
 
+// Codewars 8 kyu task Beginner Series #2 Clock
+// Description: 
+// Clock shows h hours, m minutes and s seconds after midnight.
 
+// Your task is to write a function which returns the time since midnight in milliseconds.
 
+// Example:
+// h = 0
+// m = 1
+// s = 1
 
+// result = 61000
+
+function past(h, m, s){
+    //#Happy Coding! ^_^
+  }
